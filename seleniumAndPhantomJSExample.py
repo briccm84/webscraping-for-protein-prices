ef86cf084c2263e8ff3b40ab9886cb4da1f30002
briccm84 <msbricco@gmail.com>
2022-01-17 09:54:13 -0600

    loop through product pages

--- seleniumAndPhantomJSExample.py
+++ seleniumAndPhantomJSExample.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import IPython
+import time
 
 options = Options()
 options.add_argument('--headless')
@@ -18,15 +18,16 @@ driver.get("https://us.myprotein.com/")
 
 driver.title # => "Google"
 
-driver.implicitly_wait(1000)
+driver.implicitly_wait(10000)
 
 open_search_button = driver.find_element(By.CSS_SELECTOR, "#nav > div.westendHeader > div.westendHeader_container > div.westendHeader_headerSearch > div > button > svg.headerSearch_spyglass")
 open_search_button.click()
 
 search_box = driver.find_element(By.CSS_SELECTOR, "#header-search-input")
-search_button = driver.find_element(By.CSS_SELECTOR, "#nav > div.westendHeader > div.westendHeader_container > div.westendHeader_headerSearch > div > form > div > button.headerSearch_button > svg")
+search_button = driver.find_element(By.CSS_SELECTOR, "#nav > div.westendHeader > div.westendHeader_container > div.westendHeader_headerSearch > div > form > div > button.headerSearch_button > svg")###nav > div.westendHeader > div.westendHeader_container > div.westendHeader_headerSearch > div > form > div > button.headerSearch_button > svg
 
 search_box.send_keys("whey protein powder")
+print(dir(search_button))
 search_button.click()
 
 search_result_url = driver.current_url
@@ -35,16 +36,27 @@ search_result_url = driver.current_url
 
 ul = driver.find_element(By.CSS_SELECTOR, "#mainContent > div.productListProducts > ul")
 product_list = ul.find_elements(By.CLASS_NAME, "productListProducts_product ")
-print(len(product_list))
+amount_of_products = len(product_list)
+print(amount_of_products)
 ##sub_ele = ele.find_element(By.XPATH, './div')
 ##print(sub_ele.text)
 i = 0 
-for product in product_list:
-    product_name = product.find_element(By.XPATH, "./div/a/div/h3")##/html/body/div[4]/div[1]/main/div[3]/ul/li[1]/div
-    print('{}:     {}'.format(i,product_name.text))
-    product_name.click()
-    i = i + 1
-
+while i < amount_of_products:
+    ul = driver.find_element(By.CSS_SELECTOR, "#mainContent > div.productListProducts > ul")
+    product_list = ul.find_elements(By.CLASS_NAME, "productListProducts_product ")
+    product = product_list[i]
+    product_name_element = product.find_element(By.XPATH, "./div/a/div/h3")##/html/body/div[4]/div[1]/main/div[3]/ul/li[1]/div
+    product_name = product_name_element.text
+    product.click()
+    top_row_element = driver.find_element(By.CSS_SELECTOR, "#mainContent > div.athenaProductPage_topRow")
+    price_element = top_row_element.find_element(By.CLASS_NAME, "productPrice_price")
+    price = price_element.text
+    print('{}:     {}:price: {}'.format(i,product_name,price))
+    driver.back()
+    i += 1
+print("start sleep")
+time.sleep(100.0)
+print("end wait")
 ##driver.quit()
 
 
